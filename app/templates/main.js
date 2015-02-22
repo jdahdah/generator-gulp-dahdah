6a93df2ab2010a65450fb2f3a9e708a6cb0807c0
Jerome Dahdah <parasight@gmail.com>
2015-02-22 20:59:53 +0800

    + js fixes for fastclick integration

--- app/templates/main.js
+++ app/templates/main.js
@@ -1,7 +1,9 @@
 /* jshint devel:true */
+'use strict';
 console.log('\'Allo \'Allo!');
 
 <% if (includeFastclick) { %>
+/*global FastClick */
 $(function() {
     FastClick.attach(document.body);
 });
