45abfdd8b38f9c618ed4394b483c56ac235cd5fc
Jerome Dahdah <parasight@gmail.com>
2015-03-20 18:57:05 +0100

    * shortened welcome message

--- app/index.js
+++ app/index.js
@@ -41,7 +41,7 @@ module.exports = yeoman.generators.Base.extend({
     var done = this.async();
 
     if (!this.options['skip-welcome-message']) {
-      this.log(yosay('\'Allo \'allo! Out of the box I include HTML5 Boilerplate, jQuery and a gulpfile.js to build your app. If you opt to work without Bootstrap, you even get Normalize.css as a free bonus!'));
+      this.log(yosay('\'Allo \'allo! Out of the box I include HTML5 Boilerplate, jQuery, Normalize.css and a gulpfile.js to build your app.'));
     }
 
     var prompts = [{
